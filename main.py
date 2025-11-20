73316b6f1117c3e7a0beb83d482f03df15d3ece6
Jeremy Moffitt <jeremy@localhost.localdomain>
2025-11-20 15:29:10 -0800

    leverage FreeSimpleGui imageviewer as base, update to display two images and use image events

--- main.py
+++ main.py
@@ -1,18 +1,145 @@
+#!/usr/bin/env python
 import FreeSimpleGUI as sg
+import os
+from PIL import Image, ImageTk
+import io
+import random
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [  [sg.Text('Some text on Row 1')],
-            [sg.Text('Enter something on Row 2'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+"""
+Simple Image Browser based on PySimpleGUI
+--------------------------------------------
+There are some improvements compared to the PNG browser of the repository:
+1. Paging is cyclic, i.e. automatically wraps around if file index is outside
+2. Supports all file types that are valid PIL images
+3. Limits the maximum form size to the physical screen
+4. When selecting an image from the listbox, subsequent paging uses its index
+5. Paging performance improved significantly because of using PIL
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-# Event Loop to process "events" and get the "values" of the inputs
+Dependecies
+------------
+Python3
+PIL
+"""
+
+# Get the folder containin:g the images from the user
+folder = sg.popup_get_folder('Image folder to open', default_path='')
+if not folder:
+    sg.popup_cancel('Cancelling')
+    raise SystemExit()
+
+# PIL supported image types
+img_types = (".png", ".jpg", "jpeg", ".tiff", ".bmp")
+
+# get list of files in folder
+flist0 = os.listdir(folder)
+
+# create sub list of image files (no sub folders, no wrong file types)
+fnames = [f for f in flist0 if os.path.isfile(
+    os.path.join(folder, f)) and f.lower().endswith(img_types)]
+
+num_files = len(fnames)                # number of iamges found
+if num_files == 0:
+    sg.popup('No files in folder')
+    raise SystemExit()
+
+del flist0                             # no longer needed
+
+# ------------------------------------------------------------------------------
+# use PIL to read data of one image
+# ------------------------------------------------------------------------------
+
+
+def get_img_data(f, maxsize=(1200, 850), first=False):
+    """Generate image data using PIL
+    """
+    img = Image.open(f)
+    img.thumbnail(maxsize)
+    if first:                     # tkinter is inactive the first time
+        bio = io.BytesIO()
+        img.save(bio, format="PNG")
+        del img
+        return bio.getvalue()
+    return ImageTk.PhotoImage(img)
+# ------------------------------------------------------------------------------
+
+
+# make these 2 elements outside the layout as we want to "update" them later
+# initialize to the first file in the list
+filename = os.path.join(folder, fnames[0])  # name of first file in list
+compare_filename = os.path.join(folder, fnames[0])
+if(num_files > 1):
+    compare_filename = os.path.join(folder, fnames[1])
+image_elem = sg.Image(data=get_img_data(filename, first=True), enable_events=True, key="-BASEIMG-")
+compare_image_elem = sg.Image(data=get_img_data(compare_filename, first=True), enable_events=True, key="-COMPAREIMG-")
+filename_display_elem = sg.Text(filename, size=(80, 3))
+compare_filename_display_elem = sg.Text(compare_filename, size=(80, 3))
+file_num_display_elem = sg.Text('File 1 of {}'.format(num_files), size=(15, 1))
+
+# define layout, show and read the form
+col = [[filename_display_elem],
+       [compare_image_elem]]
+
+compare_col = [[compare_filename_display_elem],
+       [image_elem]]
+
+col_files = [[sg.Listbox(values=fnames, change_submits=True, size=(60, 30), key='listbox')],
+             [sg.Button('Next', size=(8, 2)), sg.Button('Prev', size=(8, 2)), file_num_display_elem],
+             [sg.Button('Random', size=(8,2))]]
+
+layout = [[sg.Column(col_files), sg.Column(col), sg.Column(compare_col)]]
+
+window = sg.Window('Image Ranker', layout, return_keyboard_events=True,
+                   location=(0, 0), use_default_focus=False)
+
+# loop reading the user input and displaying image, filename
+i = 0
+j = 0
 while True:
+    # read the form
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    print(event, values)
+    # perform button and keyboard operations
+    if event == sg.WIN_CLOSED:
         break
-    print('You entered ', values[0])
+    elif event in ('Next', 'MouseWheel:Down', 'Down:40', 'Next:34'):
+        i += 1
+        if i >= num_files:
+            i -= num_files
+        filename = os.path.join(folder, fnames[i])
+    elif event in ('Prev', 'MouseWheel:Up', 'Up:38', 'Prior:33'):
+        i -= 1
+        if i < 0:
+            i = num_files + i
+        filename = os.path.join(folder, fnames[i])
+    elif event in ('Random'):
+        i = random.randrange(0,num_files)
+        j = random.randrange(0,num_files)
+        while(j == i):
+            j = random.randrange(0,num_files)        
+        filename = os.path.join(folder, fnames[i])
+        compare_filename = os.path.join(folder, fnames[j])
+    elif event == 'listbox':            # something from the listbox
+        f = values["listbox"][0]            # selected filename
+        filename = os.path.join(folder, f)  # read this file
+        i = fnames.index(f)                 # update running index
+    elif event == '-BASEIMG-':
+        i = random.randrange(0,num_files)
+        filename = os.path.join(folder, fnames[i])
+    elif event == '-COMPAREIMG-':
+        j = random.randrange(0,num_files)
+        while(j == i):
+            j = random.randrange(0,num_files)        
+        compare_filename = os.path.join(folder, fnames[j])        
+    else:
+        filename = os.path.join(folder, fnames[i])
+
+    # update window with new image
+    image_elem.update(data=get_img_data(filename, first=True))
+    compare_image_elem.update(data=get_img_data(compare_filename, first=True))
+    # update window with filename
+    filename_display_elem.update(filename)
+    compare_filename_display_elem.update(compare_filename)
+    # update page display
+    file_num_display_elem.update('File {} of {}'.format(i+1, num_files))
 
 window.close()
